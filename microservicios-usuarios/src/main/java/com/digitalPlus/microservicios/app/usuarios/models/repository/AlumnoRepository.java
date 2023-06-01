f51630477b235e00cf580feb83675aad7aed7957
Miller Andres Caro Gomez <mcarogom@emeal.nttdata.com>
2023-06-01 12:56:44 -0500

    se adiciona el alumnocontroller

---
+++ microservicios-usuarios/src/main/java/com/digitalPlus/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -0,0 +1,11 @@
+package com.digitalPlus.microservicios.app.usuarios.models.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.digitalPlus.microservicios.app.usuarios.models.entity.Alumno;
+
+public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
+	
+	
+	
+}
